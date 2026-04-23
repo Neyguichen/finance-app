9d9edb02324058727a97927a88600042792c8d0d
Neyguichen <larzet.s@gmail.com>
2026-04-23 15:43:31 +0200

    Desactive warning dependance manquant useEffect

--- components/AppContext.tsx
+++ components/AppContext.tsx
@@ -1,4 +1,5 @@
 'use client'
+/* eslint-disable react-hooks/exhaustive-deps */
 
 import { createContext, useContext, useEffect, useState } from 'react'
 import { createClient } from '@/lib/supabase/client'
